fce194d6e821566ac753eb96fa655702baae2286
brian50208lee <brian50208lee@gmail.com>
2019-03-03 19:45:37 +0800

    anotations, contex parm

--- src/main/java/com/brian/jerseyhello/resources/InjectDemoResource.java
+++ src/main/java/com/brian/jerseyhello/resources/InjectDemoResource.java
@@ -1,16 +1,28 @@
 package com.brian.jerseyhello.resources;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
-@Path("inject")
+@Path("/inject")
 @Consumes(MediaType.TEXT_PLAIN)
 @Produces(MediaType.TEXT_PLAIN)
 public class InjectDemoResource {
     @GET
-    public String test(@MatrixParam("matrixParm") String matrixParm,
+    @Path("/annotation")
+    public String getParmUsingAnnotation(@MatrixParam("matrixParm") String matrixParm,
                        @HeaderParam("headerParm") String headerParm,
                        @CookieParam("cookieParm") String cookieParm) {
         return "matrixParm " + matrixParm + " headerParm " + headerParm + " cookieParm " + cookieParm;
+    }
+
+    @GET
+    @Path("/context")
+    public String getParmUsingContex(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders) {
+        String absPath = uriInfo.getAbsolutePath().toString();
+        String cookies = httpHeaders.getCookies().toString();
+        return "absPath: " + absPath + " cookies: " + cookies;
     }
 }
