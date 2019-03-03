88337842f20def6efc696f23772db17b1e294539
brian50208lee <brian50208lee@gmail.com>
2019-03-03 17:27:40 +0800

    json date format

---
+++ src/main/java/com/brian/jerseyhello/json/ObjectMapperContextResolver.java
@@ -0,0 +1,22 @@
+package com.brian.jerseyhello.json;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public ObjectMapperContextResolver() {
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) { return objectMapper; }
+
+}
